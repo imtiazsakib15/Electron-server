e6d19ed7e4488ad0610f8c333e4fb5ce8ad448d8
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-02-26 17:50:42 +0600

    Get all categories api created

--- src/controllers/v1/categoryController.js
+++ src/controllers/v1/categoryController.js
@@ -1,5 +1,14 @@
 const Category = require("../../models/categoryModel");
 
+const getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).send(categories);
+  } catch (error) {
+    next(error);
+  }
+};
+
 const createCategory = async (req, res, next) => {
   try {
     const { name, image } = req.body;
@@ -12,4 +21,4 @@ const createCategory = async (req, res, next) => {
   }
 };
 
-module.exports = { createCategory };
+module.exports = { getCategories, createCategory };
--- src/routes/v1/categoryRoutes.js
+++ src/routes/v1/categoryRoutes.js
@@ -1,6 +1,11 @@
-const { createCategory } = require("../../controllers/v1/categoryController");
+const {
+  getCategories,
+  createCategory,
+} = require("../../controllers/v1/categoryController");
 
 const router = require("express").Router();
+
+router.get("/", getCategories);
 
 router.post("/", createCategory);
 
