cced9f469b24439c0c603b0d6c123a73ee4140e1
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-03-08 21:14:34 +0600

    Get a product api created

--- src/controllers/v1/productController.js
+++ src/controllers/v1/productController.js
@@ -3,7 +3,7 @@ const Product = require("../../models/productModel");
 const getProducts = async (req, res, next) => {
   try {
     const products = await Product.find({}).select(
-      "name  slug  image  price  description  category  availability"
+      "name  slug  image  price oldPrice  description  category  availability"
     );
     res.status(200).send(products);
   } catch (error) {
@@ -11,25 +11,28 @@ const getProducts = async (req, res, next) => {
   }
 };
 
+const getProduct = async (req, res, next) => {
+  try {
+    const id = req.params?.id;
+    const product = (await Product.findById(id)) || {};
+    res.status(200).send(product);
+  } catch (error) {
+    next(error);
+  }
+};
+
 const createProduct = async (req, res, next) => {
   try {
-    const { name, image, price, description, category, availability } =
-      req.body;
+    const product = req.body;
+    const { name } = product;
+
     const slug = name.toLowerCase().split(" ").join("-");
 
-    const result = await Product.create({
-      name,
-      slug,
-      image,
-      price,
-      description,
-      category,
-      availability,
-    });
+    const result = await Product.create({ ...product, slug });
     res.status(201).send(result);
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { getProducts, createProduct };
+module.exports = { getProducts, getProduct, createProduct };
--- src/routes/v1/productRoutes.js
+++ src/routes/v1/productRoutes.js
@@ -1,11 +1,14 @@
 const {
   getProducts,
+  getProduct,
   createProduct,
 } = require("../../controllers/v1/productController");
 
 const router = require("express").Router();
 
 router.get("/", getProducts);
+
+router.get("/:id", getProduct);
 
 router.post("/", createProduct);
 
