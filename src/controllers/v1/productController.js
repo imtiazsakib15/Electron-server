77140e935ae4afaee7e2522702b4d538d3515047
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-03-26 23:54:24 +0600

    Get all product controller updated

--- src/controllers/v1/productController.js
+++ src/controllers/v1/productController.js
@@ -2,9 +2,7 @@ const Product = require("../../models/productModel");
 
 const getProducts = async (req, res, next) => {
   try {
-    const products = await Product.find({}).select(
-      "name  slug  image  price oldPrice  description  category  availability"
-    );
+    const products = await Product.find({});
     res.status(200).send(products);
   } catch (error) {
     next(error);
